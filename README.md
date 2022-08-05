62d134da7c773c8aa8e42a3fd87b353910913e4c
Paul Theberge <theberge.paul@gmail.com>
2022-08-05 09:42:24 -0400

    Dropping jQuery

--- README.md
+++ README.md
@@ -1,36 +1,9 @@
 linkscroll.js
 =============
 
-####Version 2.0
-
-Short and sweet jQuery plugin which implements page scrolling for anchor tags.
-
-Setup
+Version 3.0
 -----
 
-1. Download jQuery or link within your document to a jQuery CDN.
-2. Download or clone the linkscroll.js archive into your project directory.
-3. Include the jQuery library, followed by the linkscroll script `src="js/linkscroll.min.js"`.
+Automatic scrolling for anchor tags in pure JavaScript.
 
-Use
----
-
-For links that you wish to have scroll to the location of other elements on the page:
-
-1. Add `class="linkscroll"` to the anchor tag.
-2. Set the destination of the anchor tag: `href="destination_element"`
-
-For links that you wish to have scroll to the top of the page:
-
-1. Add `class="topscroll"` to the anchor tag.
-
-Defaults / Customization
-------------------------
-
-By default, the speed of the scrolling animation is 800ms. It is very simple to change the scrolling speed however by simply adding the speed as a data attribute on the anchor tag.
-
-1. `<a href="#services" class="linkscroll" data-speed="600">Services</a>`
-
-The same applies to links which will scroll to the top of the page.
-
-1. `<a class="topscroll" data-speed="600">Back to top</a>`
+Currently only set up for anchor tags.
--- linkscroll.js
+++ linkscroll.js
@@ -1,30 +1,17 @@
-(function($) {
-    $('.linkscroll[href*=\\#]:not([href=\\#])').click(function() {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
-            var speed = $(this).attr('data-speed');
-            var target = $(this.hash);
-            if (typeof speed !== typeof undefined && speed !== false) {
-                speed = parseInt(speed, 10);
-            } else {
-                speed = 800;
-            }
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html,body').animate({
-                    scrollTop: target.offset().top
-                }, speed);
-                return false;
-            }
-        }
-    });
-    $('.topscroll').click(function(){
-        var speed = $(this).attr('data-speed');
-            if (typeof speed !== typeof undefined && speed !== false) {
-                speed = parseInt(speed, 10);
-            } else {
-                speed = 800;
-            }
-        $("html, body").animate({ scrollTop: 0 }, speed);
-        return false;
-    });
-})(jQuery);
+const links = document.querySelectorAll('a');
+
+for (const link of links) {
+  link.addEventListener('click', clickHandler);
+}
+
+function clickHandler(e) {
+  e.preventDefault();
+  
+  const href = this.getAttribute('href');
+  const offsetTop = document.querySelector(href).offsetTop;
+
+  scroll({
+    top: offsetTop,
+    behavior: 'smooth'
+  });
+}
