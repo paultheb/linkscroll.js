bb3fd8c1b7ff05eee823115db5b7540a89980d31
Paul Theberge <theberge.paul@gmail.com>
2022-08-08 11:13:11 -0400

    Updating design.

--- linkscroll.js
+++ linkscroll.js
@@ -1,17 +1,44 @@
-const links = document.querySelectorAll('a.linkscroll');
+var linkscroll = (function (window, undefined) {
 
-for (const link of links) {
-  link.addEventListener('click', clickHandler);
-}
+  let behavior = 'smooth';
 
-function clickHandler(e) {
-  e.preventDefault();
-  
-  const href = this.getAttribute('href');
-  const offsetTop = document.querySelector(href).offsetTop;
+  function toTargetHandler(e) {
+    e.preventDefault();
+    
+    const target = this.getAttribute('data-target');
+    const offsetTop = document.querySelector(target).offsetTop;
 
-  scroll({
-    top: offsetTop,
-    behavior: 'smooth'
-  });
-}
+    scroll({
+      top: offsetTop,
+      behavior: behavior
+    });
+  }
+
+  function toTopHanlder(e) {
+    e.preventDefault();
+    scroll({
+      top: 0,
+      behavior: behavior
+    })
+  }
+
+  function init(options) {
+    behavior = options.behavior;
+
+    const toTargetLinks = document.querySelectorAll('.linkscroll-target');
+    const toTopLinks = document.querySelectorAll('.linkscroll-top');
+
+    for (const link of toTargetLinks) {
+      link.addEventListener('click', toTargetHandler);
+    }
+
+    for (const link of toTopLinks) {
+      link.addEventListener('click', toTopHanlder);
+    }
+  }
+
+  return {
+    init: init
+  };
+
+})(window);
